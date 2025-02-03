395f4f171ec74fb98b28e6c1c5edaa27632cbb75
Martin Norman <martin.norman@climber.se>
2025-02-03 15:37:22 +0000

    Added grant for SYSADMIN

--- models/MasterCalendar.sql
+++ models/MasterCalendar.sql
@@ -8,7 +8,7 @@ SELECT
     DATE_PART('WEEK', DATE_DAY) WEEK, 
     DATE_PART('QUARTER', DATE_DAY) QUARTER,
     (DAY_OF_WEEK = 0 OR DAY_OF_WEEK = 6) IS_WEEKEND
-FROM ({{ dbt_utils.date_spine(datepart="day", start_date="(select min(DATE) from " + ref('Events_4') | string + ")", end_date="(select max(DATE) from " + ref('Events_4') + 1 | string + ")")}}) 
+FROM ({{ dbt_utils.date_spine(datepart="day", start_date="(select min(DATE) from " + ref('Events_4') | string + ")", end_date="(select max(DATE) from " + ref('Events_4')  | string + ")")}}) 
 
 
 
